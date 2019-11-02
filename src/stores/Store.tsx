0245e33c3919a859af691955e912950ed8a61ae4
Arkadiusz Piotrowski <arek.gabrys@gmail.com>
2019-11-02 17:26:41 +0100

    'typescript'

--- src/stores/Store.tsx
+++ src/stores/Store.tsx
@@ -4,18 +4,11 @@ import movies from './reducers/moviesReducer'
 import actors from './reducers/actorsReducer'
 import thunk from "redux-thunk";
 
-
 const rootReducer = combineReducers({ movies, actors })
-
-const composerEnhancer = composeWithDevTools({
-    realTime: true,
-    trace: true,
-    traceLimit: 25
-});
 
 const rootStore = createStore(
     rootReducer, 
-    composerEnhancer(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(thunk))
     )
 
 export default rootStore
