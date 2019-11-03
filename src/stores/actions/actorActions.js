b8ba79a34cd78603a379a0a6892bc014e880f7e0
Arkadiusz Piotrowski <arek.gabrys@gmail.com>
2019-11-03 11:05:05 +0100

    'fix'

--- src/stores/actions/actorActions.js
+++
@@ -1,8 +0,0 @@
-import { bindActionCreators } from "redux"
-import rootStore from '../Store'
-
-const addActor = item => ({type: 'ADD_ACTOR', item})
-const resetActors = () => ({type: 'RESET_ACTORS'})
-const actorsAction = bindActionCreators({add: addActor, reset: resetActors}, rootStore.dispatch)
-
-export default actorsAction
